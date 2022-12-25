239293c82d3d64d169d0d4721ab68ba29c9bed44
yraf2 <y.raf2018@yandex.ru>
2022-12-25 20:49:52 +0300

    16 домашка

--- main.py
+++ main.py
@@ -1,65 +1,145 @@
-from flask import jsonify, Flask
+from flask import jsonify, Flask, request, abort
 from flask_sqlalchemy import SQLAlchemy
 
+from config import Config
 from migrate import data_to_db
 from model import User, Order, Offer
-from config import Config
 
 app = Flask(__name__)
 db = SQLAlchemy()
 
-@app.route('/users/', methods=['GET'])
+
+@app.route('/users/', methods=['GET', 'POST'])
 def get_users():
-    try:
-        users = db.session.query(User).all()
-        return jsonify([user.serialize() for user in users])
-    except Exception as e:
-        return f'{e}'
+    if request.method == 'GET':
+        try:
+            users = db.session.query(User).all()
+            return jsonify([user.serialize() for user in users])
+        except Exception as e:
+            return f'{e}'
+    elif request.method == 'POST':
+        data = request.json
+        db.session.add(User(**data))
+        db.session.commit()
+
+        return jsonify(code=200)
 
 
-@app.route('/users/<int:user_id>', methods=['GET'])
+@app.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
 def get_user(user_id):
-    try:
+    if request.method == 'GET':
+        try:
+            user = db.session.query(User).filter(User.id == user_id).first()
+            return jsonify([user.serialize()])
+        except Exception as e:
+            return f'{e}'
+
+    elif request.method == 'PUT':
         user = db.session.query(User).filter(User.id == user_id).first()
-        return jsonify([user.serialize()])
-    except Exception as e:
-        return f'{e}'
 
-@app.route('/orders/', methods=['GET'])
+        if user is None:
+            abort(404)
+
+        db.session.query(User).filter(User.id == user_id).update(request.json)
+        db.session.commit()
+        return jsonify(code=200)
+
+    elif request.method == 'DELETE':
+        count = db.session.query(User).filter(User.id == user_id).delete()
+        db.session.commit()
+        if not count:
+            abort(404)
+        return jsonify(code=200)
+
+
+@app.route('/orders/', methods=['GET', 'POST'])
 def get_orders():
-    try:
-        orders = db.session.query(Order).all()
-        return jsonify([order.serialize() for order in orders])
-    except Exception as e:
-        return f'{e}'
+    if request.method == 'GET':
+        try:
+            orders = db.session.query(Order).all()
+            return jsonify([order.serialize() for order in orders])
+        except Exception as e:
+            return f'{e}'
+    elif request.method == 'POST':
+        data = request.json
+        db.session.add(Order(**data))
+        db.session.commit()
 
-@app.route('/orders/<int:user_id>', methods=['GET'])
+        return jsonify(code=200)
+
+
+@app.route('/orders/<int:order_id>', methods=['GET', 'PUT', 'DELETE'])
 def get_order(order_id):
-    try:
+    if request.method == 'GET':
+        try:
+            order = db.session.query(Order).filter(Order.id == order_id).first()
+            return jsonify([order.serialize()])
+        except Exception as e:
+            return f'{e}'
+
+    elif request.method == 'PUT':
         order = db.session.query(Order).filter(Order.id == order_id).first()
-        return jsonify([order.serialize()])
-    except Exception as e:
-        return f'{e}'
 
-@app.route('/offers/', methods=['GET'])
+        if order is None:
+            abort(404)
+
+        db.session.query(Order).filter(Order.id == order_id).update(request.json)
+        db.session.commit()
+        return jsonify(code=200)
+
+    elif request.method == 'DELETE':
+        count = db.session.query(Order).filter(Order.id == order_id).delete()
+        db.session.commit()
+        if not count:
+            abort(404)
+        return jsonify(code=200)
+
+
+@app.route('/offers/', methods=['GET', 'POST'])
 def get_offers():
-    try:
-        offers = db.session.query(Offer).all()
-        return jsonify([offer.serialize() for offer in offers])
-    except Exception as e:
-        return f'{e}'
+    if request.method == 'POST':
+        try:
+            offers = db.session.query(Offer).all()
+            return jsonify([offer.serialize() for offer in offers])
+        except Exception as e:
+            return f'{e}'
 
-@app.route('/offers/<int:offer_id>', methods=['GET'])
+    elif request.method == 'POST':
+        data = request.json
+        db.session.add(Offer(**data))
+        db.session.commit()
+
+        return jsonify(code=200)
+
+
+@app.route('/offers/<int:offer_id>', methods=['GET', 'PUT', 'DELETE'])
 def get_offer(offer_id):
-    try:
-        offer = db.session.query(Offer).filter(Offer.id == offer_id).first()
-        return jsonify([offer.serialize()])
-    except Exception as e:
-        return f'{e}'
+    if request.method == 'GET':
+        try:
+            offer = db.session.query(Offer).filter(Offer.id == offer_id).first()
+            return jsonify([offer.serialize()])
+        except Exception as e:
+            return f'{e}'
+
+    elif request.method == 'PUT':
+        user = db.session.query(Offer).filter(Offer.id == offer_id).first()
+
+        if user is None:
+            abort(404)
+
+        db.session.query(Offer).filter(Offer.id == offer_id).update(request.json)
+        db.session.commit()
+        return jsonify(code=200)
+
+    elif request.method == 'DELETE':
+        count = db.session.query(Offer).filter(Offer.id == offer_id).delete()
+        db.session.commit()
+        if not count:
+            abort(404)
+        return jsonify(code=200)
 
 
-if __name__== '__main__':
-    # init_db()
+if __name__ == '__main__':
     app.config.from_object(Config())
     db.init_app(app)
     with app.app_context():
